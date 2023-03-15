75b4edbb254b430cb07692004b3ac9a67b74115f
Kevin Okoth <kevinokotth04@gmail.com>
2023-03-15 22:53:28 +0300

    Add comment functionality

--- src/modules/fetchAnimes.js
+++ src/modules/fetchAnimes.js
@@ -1,7 +1,7 @@
 const fetchAnime = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const getMovie = await response.json();
-  const returnedAnimes = getMovie.slice(20, 60);
+  const returnedAnimes = getMovie.slice(100, 140);
   return returnedAnimes;
 };
 
